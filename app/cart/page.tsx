16a17dac0c007ca1465650e5b2d2c57aab68b24a
Erhan <erhan@MacBook-Air-von-Erhan.local>
2026-05-12 00:55:20 +0200

    feat: implement cart total calculation in CartPage component

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,3 +1,17 @@
+import { getCart } from "@/lib/cart";
+
+
 export default async function CartPage() {
-  return <main></main>;
+  const items = await getCart();
+
+  //Berechnet Gesamtpreis aller Cart-Items.
+  let total = 0;
+  for (const item of items) {
+    const price = Number(item.product.price);
+    const quantity = item.quantity;
+
+    total += price * quantity;
+  }
+
+  return <main></main>
 }
