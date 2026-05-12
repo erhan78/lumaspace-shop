8f9182d8cadcc4d999fade00223a2a19e0cb24c2
Erhan <erhan@MacBook-Air-von-Erhan.local>
2026-05-12 20:38:57 +0200

    feat: update Header component to display cart count dynamically

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,23 @@
 import Link from "next/link";
+import { getCartCount } from "@/lib/cart";
 
-export default function Header() {
+// Header wird auf jeder Seite angezeigt.
+export default async function Header() {
+  const cartCount = await getCartCount();
+
   return (
     <header>
+      {/* Logo / Startseite */}
       <Link href="/">Lumaspace</Link>
+
       <nav>
         <Link href="/">Shop</Link>
-        <Link href="/cart">Warenkorb</Link>
+
+        {/* Warenkorb mit Anzahl in Klammern, wenn was drin ist */}
+        <Link href="/cart">
+          Warenkorb
+          {cartCount > 0 && <span> ({cartCount})</span>}
+        </Link>
       </nav>
     </header>
   );
